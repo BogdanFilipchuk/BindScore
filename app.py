505b53095cdedcef04697cfad0fd950575277337
BabenkoAndrey <andrey.babenko@epfl.ch>
2026-05-13 16:35:35 +0200

    app modification

--- app.py
+++ app.py
@@ -2,40 +2,38 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-st.title("Binding Energy Predictor")
+#####TITLE#####
+st.title("BindScore - Binding Energy Predictor")
 
-#choosing the visualization type of the protein
+#####MAIN AREA#####
+
+### choosing the visualization type of the protein
 Protein_drawmode = st.selectbox("Protein visualization mode", ["Skeleton", "Blah", "Blah"]) 
 
 st.sidebar.slider("Temperature [K]", 0.0, 600.0, 0.7)
 
-st.write("Main content area")
-
-# pH Slider
-ph = st.slider("pH", 0.0, 14.0, 0.5)
-
-fig, ax = plt.subplots(figsize=(10, 1)) # Create gradient
-gradient = np.linspace(0, 1, 256).reshape(1, -1)
-ax.imshow(
+### pH Slider
+fig, ax = plt.subplots(figsize=(10, 0.2)) # Create a wide, short figure (horizontal line) for the pH scale
+gradient = np.linspace(0, 1, 256).reshape(1, -1) # Create gradient
+ax.imshow(  #Show the gradient in graph
     gradient,
     aspect='auto',
-    cmap='RdYlGn',
-    extent=[0, 14, 0, 1]
+    cmap='RdYlBu',
+    extent=[0.0, 14.0, 0.0, 1.0]
 )
+with st.expander("pH Scale"):
+    ph = st.slider("Choose pH", 0.0, 14.0, 7.0)
+    # Labels
+    ax.set_xticks(range(15))
+    ax.axvline(ph, color='black', linewidth=2) # Indicator line
+    st.pyplot(fig)
+    # pH Interpretation
+    if ph < 6:
+        st.error("Acidic")
+    elif 6 <= ph < 7:
+        st.success("Neutral")
+    else:
+        st.info("Basic")
 
-ax.axvline(ph, color='black', linewidth=3) # Indicator line
 
-# Labels
-ax.set_yticks([])
-ax.set_xticks(range(15))
-ax.set_xlabel("pH Scale")
 
-st.pyplot(fig)
-
-# Description
-if ph < 6.5:
-    st.error("Acidic")
-elif 6.5 <= ph < 7.5:
-    st.success("Neutral")
-else:
-    st.info("Basic / Alkaline")
