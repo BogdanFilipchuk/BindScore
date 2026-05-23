625b371daec4f9e2d99e791a2c9229953b93d5e0
FilipchukBogdan <bogdan.filipchuk@epfl.ch>
2026-05-23 12:49:18 +0200

    Work in progress

--- tests/etnropy_test.py
+++ tests/etnropy_test.py
@@ -1,6 +1,11 @@
 from bindscore.scoring import *
+from bindscore.pdb_file_treatment import pdb_utils_fetch as fetch
 import bindscore.parsing
 import pathlib as path
+from bindscore.parsing.pdb_utils_protein import Protein_Structure
 test_pdb_path:path.Path = path.Path(__file__).parent / "test_pdb_files" / "6PYH.pdb"
-total_entropy_result = compute_total_entropy(test_pdb_path,"A","D")
-print(total_entropy_result)
+# total_entropy_result = compute_total_entropy(test_pdb_path,"A","D")
+# print("total_entropy_result)
+
+myprotein = Protein_Structure(fetch.fetch_pdb_data(test_pdb_path))
+print(myprotein.get_ID)
