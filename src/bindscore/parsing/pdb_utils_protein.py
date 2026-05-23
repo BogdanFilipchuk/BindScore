0fa5d971f4f24f9c63a02d30ff8464be16b2b9d6
FilipchukBogdan <bogdan.filipchuk@epfl.ch>
2026-05-23 12:35:25 +0200

    Testing WIP

--- src/bindscore/parsing/pdb_utils_protein.py
+++ src/bindscore/parsing/pdb_utils_protein.py
@@ -27,6 +27,7 @@ class Protein_Structure:
             get_small_molecule(small_molecule_id)- Extracts the atoms for a specific small molecule.
             get_entity(entity_id)                - Extracts the atoms for a chain or small molecule.
             summary()                            - Provides a summary of the protein structure.
+            get_ID()                             - Returns the PDB ID.                           
         '''
 
         self.pdb_data  = pdb_data
--- tests/etnropy_test.py
+++ tests/etnropy_test.py
@@ -1,5 +1,6 @@
-import bindscore.scoring as sc
+from bindscore.scoring import *
 import bindscore.parsing
 import pathlib as path
-test_pdb_path:path.Path = path.Path(__file__).parent / "tests" / "6PYH.pdb"
-total_entropy_result = sc.compute_total_entropy()
+test_pdb_path:path.Path = path.Path(__file__).parent / "test_pdb_files" / "6PYH.pdb"
+total_entropy_result = compute_total_entropy(test_pdb_path,"A","D")
+print(total_entropy_result)
