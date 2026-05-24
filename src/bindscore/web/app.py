e9c89f84624188f65cfa6f0a3c50cec8db12d023
FilipchukBogdan <bogdan.filipchuk@epfl.ch>
2026-05-24 23:59:02 +0200

    Fixed app file

--- src/bindscore/web/app.py
+++ src/bindscore/web/app.py
@@ -2,13 +2,14 @@ import streamlit as st
 from bindscore.pdb_file_treatment.pdb_utils_fetch import fetch_pdb_data as fetch_pdb
 import py3Dmol
 import streamlit.components.v1 as components # if using regular st.html, gives error
-import numpy as np
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-from bindscore.scoring.total_entropy import * # to get the entropy breakdown
+import pandas as pd
+# from bindscore.scoring.total_entropy import * # to get the entropy breakdown
 from bindscore.parsing.pdb_utils_protein import Protein_Structure # to get the chains
 from bindscore.parsing.enthalpy_run import get_dataset_interaction_list # to get the interactions and binding energy differences
+
 # auxiliary functions and tools
 chain_colors = [
     '#e6194b', '#3cb44b', '#4363d8', '#f58231',
@@ -34,6 +35,17 @@ def get_interaction_color(energy, norm):
     )
     return mcolors.to_hex(cmap(norm(energy)))  # Map the energy to a color using the colormap and normalization
 
+def get_binding_energy_summary(interactions: list[dict]) -> pd.DataFrame:
+    df = pd.DataFrame(interactions)
+    summary = (
+        df.groupby("interaction type")["binding energy"]
+        .sum()
+        .reset_index()
+        .rename(columns={"interaction type": "Interaction Type", "binding energy": "Total Binding Energy"})
+        .sort_values("Total Binding Energy")
+    )
+    return summary
+
 #####TITLE#####
 st.title("BindScore - Binding Energy Predictor")
 
@@ -42,7 +54,8 @@ st.title("BindScore - Binding Energy Predictor")
 
 #####MAIN AREA#####
 ### protein input and visualization
-protein_input = fetch_pdb(st.text_input("Enter protein PDB RCSB database ID (e.g. 1A2B)", "1A2B"))  # input the protein PDB ID
+protein_input_name = st.text_input("Enter protein PDB RCSB database ID (e.g. 1A2B)", "1A2B")
+protein_input = fetch_pdb(protein_input_name)  # input the protein PDB ID
 protein_input_parsed = Protein_Structure(protein_input)  # turn data into a Protein_Structure object
 chains = protein_input_parsed.chains()  # get the chains for interaction analysis
 # residues_importance = calculate_residue_importance(protein_input)
@@ -90,11 +103,10 @@ with col2:
 ### interaction calculation and gradient coloring  
 if st.button("Analyze the interactions"):               # Button to start the interaction analysis
     st.spinner(text="Calculating...", width="content")  # Show spinner while processing
-    interactions_full = get_dataset_interaction_list(protein_input, chain_1, chain_2)# Calculate interactions and binding energy differences
+    interactions_full = get_dataset_interaction_list(protein_input_name, chain_1, chain_2)# Calculate interactions and binding energy differences
     energy_values = []
     for interaction in interactions_full:
-        energy_values.append(interaction["binding energy"]) # Collect binding energy values for normalization to make the color gradient
-
+        energy_values.append(interaction["binding energy"])
     norm = mcolors.TwoSlopeNorm(                        # Normalize the binding energy around zero
             vmin=min(energy_values), vcenter=0.0, vmax=max(energy_values))
     
@@ -103,9 +115,11 @@ if st.button("Analyze the interactions"):               # Button to start the in
 
     for interaction in interactions_full:                          
         if Protein_drawmode == "cartoon":                                
+            viewer.setStyle( {'cartoon': {'color': "black"}}) # Reset all to white first to make the interaction colors stand out
             viewer.setStyle({'resi': interaction["atom1"]["residue_seq"]}, {'cartoon': {'color': get_interaction_color(interaction["binding energy"], norm) }})
             viewer.setStyle({'resi': interaction["atom2"]["residue_seq"]}, {'cartoon': {'color': get_interaction_color(interaction["binding energy"], norm) }})
         else:                                               
+            viewer.setStyle( {'stick': {'color': "black"}}) # Reset all to white first to make the interaction colors stand out
             viewer.setStyle({'resi': interaction["atom1"]["residue_seq"]}, {'stick': {'color': get_interaction_color(interaction["binding energy"], norm) }})
             viewer.setStyle({'resi': interaction["atom2"]["residue_seq"]}, {'stick': {'color': get_interaction_color(interaction["binding energy"], norm) }})
 
@@ -113,22 +127,22 @@ if st.button("Analyze the interactions"):               # Button to start the in
     html = viewer._make_html()                          # Render in Streamlit
     components.html(html, width=750, height=750)        # Size of the streamlit component
 
-### Interactions table  MAKE A NICE TABLE AND JUST INSERT IT INTO ST.DATAFRAME 
-   
-st.dataframe(data)  # Display interactions in a table format
+### Interactions table
+    enthalpy_data = get_binding_energy_summary(interactions_full)  # Prepare the interaction data for display in a table format
+    st.dataframe(enthalpy_data)                                    # Display interactions in a table format
 
-### Entropy table
-results = compute_total_entropy(
-    complex_pdb = protein_input, 
-    chain_a = chain_1, 
-    chain_b = chain_2, 
-    return_breakdown = True
-)
-results_dict = {"Entropy type": "Free Energy contribution (J/(mol))",
-    "Translational-Rotational": results.dS_trans_rot, 
-    "Hydrophobic": results.dS_hydrophobic, 
-    "Side-chain": results.dS_sidechain, 
-    "Backbone": results.dS_backbone, 
-    "Total": results.dS_total
-}
-st.table(results_dict)
+### Entropy table - the entropy calculations turned out to be very complicated.
+# results = compute_total_entropy(
+#     complex_pdb = protein_input, 
+#     chain_a = chain_1, 
+#     chain_b = chain_2, 
+#     return_breakdown = True
+# )
+# results_dict = {"Entropy type": "Free Energy contribution (J/(mol))",
+#     "Translational-Rotational": results.dS_trans_rot, 
+#     "Hydrophobic": results.dS_hydrophobic, 
+#     "Side-chain": results.dS_sidechain, 
+#     "Backbone": results.dS_backbone, 
+#     "Total": results.dS_total
+# }
+# st.table(results_dict)
